b3ba017b5859262c456322c89da52e6d161d8dd4
Berkay Tuncel <berkaytncl98@gmail.com>
2023-02-09 03:09:27 +0300

    Add endpoints to Constants file

--- GraduationProject/entity/Constants.swift
+++ GraduationProject/entity/Constants.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 enum Constants {
+    private static let baseURL = "http://kasimadalan.pe.hu/yemekler"
+    
+    static let bringAllFood = "\(baseURL)/tumYemekleriGetir.php"
+    static let bringTheBasket = "\(baseURL)/sepettekiYemekleriGetir.php"
+    static let addToBasket = "\(baseURL)/sepeteYemekEkle.php"
+    static let deleteFromBasket = "\(baseURL)/sepettenYemekSil.php"
+    static func image(imageName: String) -> String {
+        "\(baseURL)/resimler/\(imageName)"
+    }
+    
     static let ok = "OK"
     static let error = "Error"
     static let enterValidMailOrPassword = "Please enter valid email or password!"
