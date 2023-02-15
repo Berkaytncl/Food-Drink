44d72ef0c02d39dff619cb9da76fc05c0431ca0a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-02-15 19:06:18 +0300

    Add extension for screen size

---
+++ GraduationProject/extensions/CGFloat+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+Ext.swift
+//  GraduationProject
+//
+//  Created by Berkay Tuncel on 10.02.2023.
+//
+
+import UIKit
+
+extension CGFloat {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
