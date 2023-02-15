ccf648ec104af83c0dfbae0a5169e344db624cdd
Berkay Tuncel <berkaytncl98@gmail.com>
2023-02-15 18:58:47 +0300

    Add constants

--- GraduationProject/entity/Constants.swift
+++ GraduationProject/entity/Constants.swift
@@ -7,15 +7,30 @@
 
 import Foundation
 
-enum Constants {
-    private static let baseURL = "http://kasimadalan.pe.hu/yemekler"
+struct Constants {
     
-    static let bringAllFood = "\(baseURL)/tumYemekleriGetir.php"
-    static let bringTheBasket = "\(baseURL)/sepettekiYemekleriGetir.php"
-    static let addToBasket = "\(baseURL)/sepeteYemekEkle.php"
-    static let deleteFromBasket = "\(baseURL)/sepettenYemekSil.php"
-    static func image(imageName: String) -> String {
-        "\(baseURL)/resimler/\(imageName)"
+    struct URLs {
+        private static let baseURL = "http://kasimadalan.pe.hu/yemekler"
+        
+        static let bringAllFoodURL = "\(baseURL)/tumYemekleriGetir.php"
+        static let bringTheBasketURL = "\(baseURL)/sepettekiYemekleriGetir.php"
+        static let addToBasketURL = "\(baseURL)/sepeteYemekEkle.php"
+        static let deleteFromBasketURL = "\(baseURL)/sepettenYemekSil.php"
+        static func downloadImageURL(image: String) -> String { "\(baseURL)/resimler/\(image)" }
+    }
+    
+    struct Colors {
+        static let backgroundColor = "backgroundColor"
+    }
+    
+    struct Destination {
+        static let toHomeScreen = "toHomeScreen"
+        static let toWatchDeliveryScreen = "toWatchDeliveryScreen"
+        static let toLoginScreen = "toLoginScreen"
+    }
+    
+    struct CellReuseID {
+        static let mealCell = "MealCell"
     }
     
     static let ok = "OK"
